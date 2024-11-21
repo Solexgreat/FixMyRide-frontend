b6298a8f8890c4a960759f44242f94a57a9fcc3e
Solexgreat <solomonGodkid@gmail.com>
2024-11-21 16:31:24 +0100

    FxMyRide

--- new_frontend/src/components/Appointments.js
+++ new_frontend/src/components/Appointments.js
@@ -4,6 +4,8 @@ function Appointments() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [availableMechanics, setAvailableMechanics] = useState([]);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
@@ -12,6 +14,47 @@ function Appointments() {
             .then(data => setTimeSlots(data))
             .catch(error => console.error('Error fetching time slots:', error));
     }, []);
+
+    const fetchAvailableTime = async () => {
+        try{
+            const response = await fetch(
+                `http://localhost:3000/appointments/available-slots?date=${date}`
+            );
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || "failed to fetch data")
+            }
+
+            const data = await response.json();
+            setAvailableSlot(data.available_slot);
+            setError(null);
+        } catch (err) {
+            setError(err.message);
+            setAvailableSlot([])
+        }
+    }
+
+    const fetchAvailableMechanics = async () => {
+        try {
+          const response = await fetch(
+            `http://localhost:3000/appointments/available-mechanics?date=${date}&time=${time}`
+          );
+
+          if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.error || "Failed to fetch data");
+          }
+
+          const data = await response.json();
+          setAvailableMechanics(data.available_mechanics);
+          setError(null); // Clear previous errors
+        } catch (err) {
+          setError(err.message);
+          setAvailableMechanics([]); // Clear results if there's an error
+        }
+      };
+
 
   return (
     <section>
@@ -52,11 +95,30 @@ function Appointments() {
                         </option>
                     </select>
 
-                    <label htmlFor='date'>Appointment date</label>
-                    <select id='date' type='date' required >
-                        <option>
-                            {}
-                        </option>
+                    <label htmlFor='date'>
+                        Select date:
+                        <input
+                        id='date'
+                        type='date'
+                        value={date}
+                         />
+                    </label>
+
+                </div>
+
+                <div className='available-mechanics'>
+                    <label htmlFor='select-mechanics'></label>
+                    <select id='select-mechanics' type=''>
+                            {availableMechanics.length > 0 ? (
+                                {
+                                    availableMechanics.map((mechanics) => (
+                                        <option>
+                                        {mechanics.name}
+                                        </option>
+                                    ))
+                                }
+                            )
+                            }
                     </select>
                 </div>
 
